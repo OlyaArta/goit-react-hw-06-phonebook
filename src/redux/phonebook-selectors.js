2db96b210a0e5a3af4a4d0f83040987f31fc7c4e
OlyaArta <artanika8@gmail.com>
2021-11-11 12:14:53 +0200

    fix

--- src/redux/phonebook-selectors.js
+++ src/redux/phonebook-selectors.js
@@ -1,16 +1,12 @@
 export const getFilter = (state) => state.contacts.filter;
-export const getContacts = (state) => {
-  // console.log(state.contacts.items);
-  return state.contacts.items;
-};
+export const getContacts = (state) => state.contacts.items;
 
 export const getVisibleContacts = (state) => {
-  // console.log(state);
   const filter = getFilter(state);
   const contacts = getContacts(state);
   console.log(contacts);
   const normalizedFilter = filter.toLowerCase();
-  // console.log(filter);
+  console.log(filter);
 
   return contacts.filter((contact) => {
     // console.log(contact.name.name);
